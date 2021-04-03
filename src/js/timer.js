97a3a61b72d900fe434fb22f4aec8f47fc66b5d7
sergio9815 <gsergio1598@gmail.com>
2021-04-03 03:36:31 -0500

    Actualizando módulo timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -5,6 +5,7 @@ const play = document.querySelector('#btn-Start')
 play.addEventListener('click', (e) => {
   const init = new Timer({
     idContainer: 'timerJS',  //USE HTML ELEMENT ID
+    timerFormat: '00:00:00' 
   });
 
   init.start();
